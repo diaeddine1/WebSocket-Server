2fd22f7fb0571bd68ce094b50cc232248045f9fe
dia eddine <diaeddinee@outlook.fr>
2024-12-01 23:18:57 +0100

    Added the gemini code

--- app.py
+++ app.py
@@ -1,30 +1,38 @@
-#!/usr/bin/env python
-
 import asyncio
 import http
 import signal
+
 from websockets.asyncio.server import serve
 
-async def echo(websocket, path):
+
+async def echo(websocket):
     async for message in websocket:
         await websocket.send(message)
 
-async def health_check(path):
-    return path == "/healthz"
 
-async def handler(websocket, path):
-    if await health_check(path):
-        await websocket.send("OK\n")
-    else:
-        await echo(websocket, path)
+def health_check(connection, request):
+    if request.path == "/healthz":
+        # Accept both GET and HEAD requests for health check
+        if request.method in ("GET", "HEAD"):
+            return connection.respond(http.HTTPStatus.OK, "OK\n")
+        else:
+            return connection.respond(http.HTTPStatus.METHOD_NOT_ALLOWED)
+
 
 async def main():
+    # Set the stop condition when receiving SIGTERM.
     loop = asyncio.get_running_loop()
     stop = loop.create_future()
     loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
 
-    async with serve(handler, host="", port=8080):
+    async with serve(
+        echo,
+        host="",
+        port=8080,
+        process_request=health_check,
+    ):
         await stop
+
 
 if __name__ == "__main__":
     asyncio.run(main())
